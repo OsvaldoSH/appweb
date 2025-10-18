6e0e58fc471560e741bc57f612efeae3856afc44
OsvaldoSH <95552831+OsvaldoSH@users.noreply.github.com>
2025-10-18 00:28:33 -0600

    Estructura de paginas

---
+++ frontend/src/pages/ControlVacio/ControlVacio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './ControVacio.css';
+
+const ControlVacio = () => {
+    return (
+        <div className="control-vacio-page">
+            <h1>Control de Vacio</h1>
+            <p>Pagina en construccion...</p>
+        </div>
+    );
+};
+
+export default ControlVacio;
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home-page">
+            <h1>Distribuidora de Cerveza Zacapoaxtla</h1>
+            <p>Bienvenidos al sistema de gestion</p>
+        </div>
+    );
+};
+
+export default Home;
